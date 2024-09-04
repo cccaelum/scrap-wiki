2517f544ca8db73b0106b9be525ed9e6b5435ec6
cccaelum <ccc.trad@gmail.com>
2024-09-04 08:05:14 +0200

    'enlaces'

--- index.js
+++ index.js
@@ -5,41 +5,60 @@ const app = express()
 
 const url = 'https://es.wikipedia.org/wiki/Categor%C3%ADa:M%C3%BAsicos_de_rap'
 
-app.get('/', (req, res) => {
-    axios.get(url).then((response) => {
-        if(response.status === 200) {
-            const html = response.data
-            const $ = cheerio.load(html)
+app.get('/', async (req, res) => {
+    try {
+        const response = await axios.get(url);
+        if (response.status === 200) {
+            const html = response.data;
+            const $ = cheerio.load(html);
 
-            const pageTitle = $('title').text()
+            // Extraer los enlaces y nombres de los músicos de rap
+            const musicosDeRap = [];
+            $('#mw-pages a').each((index, element) => {
+                const name = $(element).text();
+                const link = $(element).attr('href');
+                if (link.startsWith('/wiki/')) {
+                    musicosDeRap.push({
+                        name,
+                        link: link.replace('/wiki/', ''), // Guardar solo la parte final del enlace
+                    });
+                }
+            });
 
-            const links = [];
-            const imgs = [];
-
-            $('a').each((index, element) => {
-                const link = $(element).attr('href')
-                links.push(link)
-            })
-
-            $('img').each((index, element) => {
-                const img = $(element).attr('src')
-                imgs.push(img)
-            })
-
+            // Renderizar la lista de músicos de rap con enlaces
             res.send(`
-                <h1>${pageTitle}</h1>
-                <h2>Enlaces</h2>
+                <h1>Músicos de Rap en Wikipedia</h1>
                 <ul>
-                    ${links.map(link => `<li><a href="${url}${link}">${link}</a></li>`).join('')}
+                    ${musicosDeRap.map(musico => `
+                        <li>
+                            <a href="/musico-de-rap/${encodeURIComponent(musico.link)}">${musico.name}</a>
+                        </li>
+                    `).join('')}
                 </ul>
-                <h2>Imagenes</h2>
-                <ul>
-                    ${imgs.map(img => `<li><a href="${url}${img}">${img}</a></li>`).join('')}
-                </ul>
-                `)
+            `);
         }
-    })
-})
+    } catch (error) {
+        console.error('Error al hacer scraping:', error);
+        res.status(500).send('Hubo un error al realizar el scraping.');
+    }
+});
+
+// Ruta dinámica para mostrar información de cada músico de rap
+app.get('/musico-de-rap/:id', async (req, res) => {
+    const musicoId = req.params.id;
+    const musicoUrl = `https://es.wikipedia.org/wiki/${musicoId}`;
+
+    try {
+        const response = await axios.get(musicoUrl);
+        if (response.status === 200) {
+            const html = response.data;
+            res.send(html);
+        }
+    } catch (error) {
+        console.error('Error al acceder a la página del músico de rap:', error);
+        res.status(500).send('Hubo un error al acceder a la página del músico de rap.');
+    }
+});
 
 app.listen(3000, () => {
     console.log('express esta escuchando en el puerto http://localhost:3000')
